5e25bc7594d6cf9bbe59cb373ecc4f76bc035608
Lavínia Souza <110833526+souzalavs@users.noreply.github.com>
2023-03-29 17:12:37 -0300

    subindo o projeto

---
+++ src/stickers/App.java
@@ -0,0 +1,29 @@
+package stickers;
+
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+	public static void main(String[] args) throws Exception {
+		
+		URI endereco = URI.create("https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json");
+		var client = HttpClient.newHttpClient();
+		var request = HttpRequest.newBuilder(endereco).GET().build();
+		HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+		String body = response.body();
+		
+		var parser = new JsonParser();
+		List<Map<String, String>> listaDeFilmes = parser.parse(body);
+		
+		for (Map<String, String> filme : listaDeFilmes) {
+			System.out.println(filme.get("title"));
+			System.out.println(filme.get("image"));
+			System.out.println(filme.get("imDbRating"));
+		}
+	}
+}
